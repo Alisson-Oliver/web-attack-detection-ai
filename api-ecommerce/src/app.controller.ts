c46486cd248998d1260cfb12f64e90135e52d629
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2026-06-05 22:14:32 -0300

    enchament: update api files

--- api-ecommerce/src/app.controller.ts
+++ api-ecommerce/src/app.controller.ts
@@ -1,12 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Post, Param, UnauthorizedException } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
+  
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return "E-commerce Rodando!";
+  }
+
+  @Get('api/produtos')
+  getProdutos() { return "Lista de Produtos"; }
+
+  @Get('api/jogos')
+  getJogos() { return "Lista de Jogos"; }
+
+  @Get('api/softwares')
+  getSoftwares() { return "Lista de Softwares"; }
+
+  @Get('api/promocoes')
+  getPromocoes() { return "Promoções"; }
+
+  @Get('api/lancamentos')
+  getLancamentos() { return "Lançamentos"; }
+
+  @Get('api/produtos/:id')
+  getProdutoById(@Param('id') id: string) { return `Produto ${id}`; }
+
+  @Post('api/login')
+  fazerLogin() {
+    throw new UnauthorizedException('Credenciais inválidas');
   }
 }
